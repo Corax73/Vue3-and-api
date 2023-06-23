9cbb99cd32fbf4fdc02b835dff58350c5d371e4a
Corax73 <ramaz999@inbox.ru>
2023-06-23 23:58:35 +0300

    added output data from the api in the component

--- vuelearn/src/App.vue
+++ vuelearn/src/App.vue
@@ -2,22 +2,27 @@
   <div>
     <span>Work!</span>
     <button @click="getUsers">Get</button>
+    <UserList :users="users"/>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import UserList from './components/UserList.vue';
   export default {
+    components: {
+      UserList
+    },
     data() {
       return {
-
+        users: []
       }
     },
     methods: {
       async getUsers() {
         try {
           const response = await axios.get('http://localhost:8000/');
-          console.log(response);
+          this.users = response.data;
         } catch(e) {
           alert('Error!');
         }
